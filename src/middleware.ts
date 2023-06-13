b0a9a377a2ef3978147e863a74ec4a85258c9715
qiyanlong <qiyanlong@qie.tv>
2023-06-13 11:45:38 +0800

    feat: filter path middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,21 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
+    console.log('NEXT_URL:', request.url);
     return NextResponse.next({
         headers: { Server: 'qie-fe-group', 'client-ip': request.ip ?? '-' }
     });
 }
+
+export const config = {
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         */
+        '/((?!api|_next/static|_next/image|favicon.ico).*)'
+    ]
+};
