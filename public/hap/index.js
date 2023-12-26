d2e021ad8437827199d047dc6f595e2d5f161f95
qiyanlong <qiyanlong@qie.tv>
2023-12-26 23:51:40 +0800

    feat: m3u8 stream

--- public/hap/index.js
+++ public/hap/index.js
@@ -1,9 +1,8 @@
 (function () {
-    const answer = document.querySelector(".answer");
-    const video = document.querySelector("video");
-    const audio = document.querySelector("audio");
+    const answer = document.querySelector('.answer');
+    const video = document.querySelector('video');
+    const audio = document.querySelector('audio');
     const img = document.querySelector('img');
-
 
     // document.querySelector(".live-wraper").addEventListener("click", () => {
     //     document.querySelector("main").requestFullscreen();
@@ -11,26 +10,52 @@
 
     img.onclick = () => {
         audio.play();
-        document.querySelector("main").requestFullscreen();
+        document.querySelector('main').requestFullscreen();
         img.onclick = null;
+    };
+
+    video.onloadedmetadata = () => {
+        video.play().catch(console.warn);
+        video.onloadedmetadata = null;
     }
 
-    answer.addEventListener("click", () => {
+    answer.addEventListener('click', () => {
         answer.remove();
         audio.pause();
         document.querySelector('img').remove();
         startlive(() => {
-            video.play().catch(console.log);
+            // video.play().catch(console.log);
+
+            var videoSrc = 'stream/play.m3u8';
+            if (Hls.isSupported()) {
+                var hls = new Hls();
+                hls.loadSource(videoSrc);
+                hls.attachMedia(video);
+            }
+            // HLS.js is not supported on platforms that do not have Media Source
+            // Extensions (MSE) enabled.
+            //
+            // When the browser has built-in HLS support (check using `canPlayType`),
+            // we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video
+            // element through the `src` property. This is using the built-in support
+            // of the plain video element, without using HLS.js.
+            else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+                video.src = videoSrc;
+            }
         });
     });
 
     function startlive(fn) {
-        // console.log(fn);
+        if (!navigator.mediaDevices) {
+            console.warn('启动摄像头失败')
+            fn();
+            return
+        }
         navigator.mediaDevices
-            .getUserMedia({ video: { facingMode: "user" } })
-            .then((stream) => {
-                const streaming = document.querySelector(".streaming");
-                if ("srcObject" in streaming) {
+            .getUserMedia({ video: { facingMode: 'user' } })
+            .then(stream => {
+                const streaming = document.querySelector('.streaming');
+                if ('srcObject' in streaming) {
                     streaming.srcObject = stream;
                 } else {
                     video.src = window.URL.createObjectURL(stream);
@@ -40,21 +65,24 @@
                     fn();
                 };
 
-                video.ontimeupdate = (e) => {
+                video.ontimeupdate = e => {
                     console.log(video.currentTime);
                     if (video.currentTime > 9) {
                         video.ontimeupdate = null;
-                        document.querySelector(".live-wraper").remove();
+                        document.querySelector('.live-wraper').remove();
                         //.classList.add('hide');
 
                         stream.getTracks().forEach(track => {
                             track.stop();
-                        })
+                        });
                     } else if (video.currentTime > 3) {
-                        document.querySelector(".live-wraper").classList.remove('hide');
+                        document.querySelector('.live-wraper').classList.remove('hide');
                     }
                 };
             })
-            .catch(console.warn);
+            .catch(() => {
+                console.warn('启动摄像头失败')
+                fn();
+            });
     }
 })();
